8c45d8f9d6a9d8c95161a94dc819b330acf05638
Marine ALLIONE <mallione@greta42.tech>
2019-11-13 09:13:17 +0100

    creation boucle while

--- tableau.php
+++ tableau.php
@@ -7,5 +7,15 @@ echo "2eme elements " .$a[1]. "\n" ;
 echo "Nombre elements " .count($a). "\n" ;
 
 $a[] = 28;
+$a[5] = 32;
+//$a[7] = 32;
 
 print_r($a);
+
+// foreach, for, while
+
+$i = 0;
+while ($i < count($a)) {
+  echo "acces direct ".$i." element : ".$a[$i]. "\n";
+  $i++; //$i += 1, $i = $i + 1
+}
