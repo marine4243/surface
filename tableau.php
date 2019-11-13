a410cb74a864ea97ef1df7c5753c1b130557df89
Marine ALLIONE <mallione@greta42.tech>
2019-11-13 09:52:23 +0100

    suprimer element d'un tableau -unset-

--- tableau.php
+++ tableau.php
@@ -4,16 +4,19 @@ $a = [1,13,17,5];
 
 echo "2eme elements " .$a[1]. "\n" ;
 
-echo "Nombre elements " .count($a). "\n" ;
-
 $a[] = 28;
 $a[5] = 32;
-//$a[7] = 32;
+$a[7] = 32;
+$a[] = 90;
+
+//pour supprimer un element du tableau
+unset($a[7]);
+
+echo "Nombre elements " .count($a). "\n" ;
 
 print_r($a);
 
 // foreach, for, while
-
 $i = 0;
 
 echo "boucle while \n";
